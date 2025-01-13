ba59a87ffd9521b07f0edb22fef55a25af815adc
TejaYeshamoni <yteja01@gmail.com>
2025-01-13 10:13:30 +0530

    updates

---
+++ frontend/src/services/transactionService.js
@@ -0,0 +1,24 @@
+
+import axios from "axios";
+import { Base_url } from "../constants";
+
+export const fetchTransactionsService = async ({ status, startDate, endDate }) => {
+  const token = localStorage.getItem("token");
+  try {
+    const { data } = await axios.get(`${Base_url}/api/transactions`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        status,
+        startDate: startDate ? startDate.toISOString() : null,
+        endDate: endDate ? endDate.toISOString() : null,
+      },
+    });
+    return data; 
+  } catch (error) {
+    console.error("Error fetching transactions:", error);
+    throw error;
+  }
+};
